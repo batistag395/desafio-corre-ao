64dedb939022b6c57e68b1bac3d49de0771b2056
gustavo <batistag395@gmail.com>
2020-01-16 16:17:59 -0300

    Criando arq. HTML para empresa

--- empresa/models.py
+++ empresa/models.py
@@ -3,7 +3,19 @@ from django.db import models
 # Create your models here.
 class Empresa(models.Model):
     nome = models.CharField(max_length=255, verbose_name='Nome')
-    cnpj = models.CharField(max_length=255, verbose_name='CPF')
+    area=(
+        ('H', 'Saúde'),
+        ('T', 'Tecnologia'),
+        ('E', 'Construção Civil'),
+        ('L', 'Logistica'),
+        ('M', 'Metalurgica'),
+        ('S', 'Segurança')
+    )
+    Area_de_Atuação = models.CharField(max_length=100)
+    cnpj = models.IntegerField()
     email = models.EmailField(max_length=255, verbose_name='E-mail')
-    telefone = models.CharField(max_length=255, verbose_name='Telefone')
+    telefone = models.IntegerField()
     ativo = models.BooleanField(default=True)
+
+def __str__(self):
+    return self.nome
--- empresa/views.py
+++ empresa/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def cadastro(request):
+    form = EmpresaForm(request.POST or None)
+    args = {
+        'form':form
+    }
+    if form.is_valid():
+        form.save()
+        args['msg'] = 'Cadastro Realizado com sucesso'
+    return render(request, 'cad-Empresa.html', args)
